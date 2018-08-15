09b6e691d73e3d4fe663936cb0552c8bd0cbcc2d
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-15 10:04:05 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,13 +2,13 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
-# class Kid include Dance
-#   attr_accessor :name
+class Kid include Dance
+  attr_accessor :name
  
-#   def initialize(name)
-#     @name = name
-#   end
-# end
+  def initialize(name)
+    @name = name
+  end
+end
 
 class Kid
   include FancyDance::InstanceMethods
